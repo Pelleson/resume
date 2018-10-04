e7977aadfc78da47aed7553046246b0d8d1824da
Pelle Alstermo <pelle@Pelles-MacBook-Pro.local>
2018-10-04 12:30:01 +0200

    npm packages fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import { IntlProvider, addLocaleData } from 'react-intl';
+import * as moment from 'moment';
 
 addLocaleData(require('react-intl/locale-data/sv'));
 addLocaleData(require('react-intl/locale-data/en'));
@@ -10,6 +11,7 @@ addLocaleData(require('react-intl/locale-data/en'));
 const locale: string = "sv-SE";
 const localePrefix = locale.split("-")[0];
 const messages = require(`./translations/${localePrefix}`).default;
+moment.locale(locale);
 
 ReactDOM.render(
 	<IntlProvider messages={messages} locale={localePrefix} >
